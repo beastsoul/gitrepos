6bfe05b21da0d5cdc9dedc4f35f645df30dce360
beastsoul <robert.schielek@gmx.de>
2016-11-13 21:53:45 +0100

    added projekt
    added project

---
+++ src/main/java/rschi/test/HelloController.java
@@ -0,0 +1,16 @@
+package rschi.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello(@RequestParam String name){
+		
+		return "hello"+name;
+	}
+
+}
